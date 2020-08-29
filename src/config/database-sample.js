95789dc7b7e1d587dc6e231caf6d95724a3c4a91
barrsofilho <barrosofilho.ti@gmail.com>
2020-08-29 17:38:26 -0300

    Correção do espaçamento dos botões

--- src/config/database-sample.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg')
-
-module.exports = new Pool({
-    user: "postgres",
-    password:"senha-do-banco",
-    host:"localhost",
-    port:5432,
-    database:"nome-do-banco"
-})
